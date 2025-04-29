bb15bfa95274156f387a73248c0704cab648a0e1
Vishal Pasi <vishalnpasi@gmail.com>
2025-04-29 21:58:55 +0530

    init commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,17 @@
-//package org.example;
-//
-//
-//import org.example.abstraction.Animal;
-//import org.example.abstraction.Dog;
-//import org.example.abstraction.Cat;
-//
-//public class Main {
-//    public static void main(String[] args) {
-//        System.out.println("Java-programming-playground");
-//        Cat cat = new Cat();
-//        cat.showAnimal();
-//        cat.printAnimalVoice();
-//        cat.catOnly();
-//    }
-//}
+package org.example;
+
+
+import org.example.abstraction.Animal;
+import org.example.abstraction.Dog;
+import org.example.abstraction.Cat;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("*****************************Java-programming-playground***********************");
+        Cat cat = new Cat();
+        cat.showAnimal();
+        cat.printAnimalVoice();
+        cat.catOnly();
+
+    }
+}
